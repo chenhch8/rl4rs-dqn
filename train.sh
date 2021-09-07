a6fa2a96e7fe979bb8bc36731f2c9738731582ba
chenhch8 <chenhch8@mail2.sysu.edu.cn>
2021-09-07 20:25:51 +0800

    Update train.sh
    update

--- train.sh
+++ train.sh
@@ -5,6 +5,6 @@ DUELING=0
 #rm $OUTDIR/train.log
 #nohup CUDA_VISIBLE_DEVICES="0,1" python3 -u src/train.py --outdir "./output_env1"  > output_env1/train.log 2>&1 | tail -F output_env1/train.log &
 #CUDA_VISIBLE_DEVICES="-1" python3 -u src2/train.py --outdir $OUTDIR --dueling $DUELING  >> $OUTDIR/train.log 2>&1 | tail -F $OUTDIR/train.log
-CUDA_VISIBLE_DEVICES="0" python3 src/train.py --outdir $OUTDIR --dueling $DUELING >> $OUTDIR/train.log 2>&1 &
+CUDA_VISIBLE_DEVICES="0" python3 -u src/train.py --outdir $OUTDIR --dueling $DUELING >> $OUTDIR/train.log 2>&1 &
 tail -F $OUTDIR/train.log
 #CUDA_VISIBLE_DEVICES="0" python3 src/train.py --outdir $OUTDIR --dueling $DUELING
